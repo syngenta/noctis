c79dadffe73d3c771032459f54f1cfc1bf96c5b9
Nataliya Lopanitsyna <31595244+NatalyLo@users.noreply.github.com>
2025-05-09 09:10:31 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,11 +5,6 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import noctis
-import os
-import sys
-
-#sys.path.insert(0, os.path.abspath("."))
 
 
 project = "noctis"
@@ -35,7 +30,7 @@ exclude_patterns = ["build", "Thumbs.db", ".DS_Store"]
 autosummary_generate = True
 
 
-version = noctis.__version__
+version = "1.0.3"
 # The full version, including dev info
 release = version.replace("_", "")
 
